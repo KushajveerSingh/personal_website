77182531781ba8696858e7e93c054d599e2c412e
KushajveerSingh <kushajreal@gmail.com>
2023-03-28 01:30:21 -0400

    add deploy action

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,6 +20,7 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'KushajveerSingh', // Usually your GitHub org/user name.
   projectName: 'personal_website', // Usually your repo name.
+  trailingSlash: false,
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'throw',
